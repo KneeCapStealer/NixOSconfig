e6aa9cb3f9f0db996c39eda91963ea70a9886455
Christoffer Hald Christensen <chrishald@proton.me>
2025-01-05 23:18:31 +0100

    Added home-manager

--- flake.nix
+++ flake.nix
@@ -3,14 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs } @ inputs: {
+  outputs = { nixpkgs, home-manager, ... } @ inputs: {
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
 
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.default
+        {
+          home-manager.useUserPackages = true;
+          home-manager.useGlobalPkgs = true;
+        }
       ];
     };
   };
