ae538446ada81616f3483de450c8836a4ea3d124
Christoffer Hald Christensen <chrishald@proton.me>
2025-01-05 23:10:50 +0100

    Updated flake description and added desktop configuration

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
-  description = "A very basic flake";
+  description = "A collection of my nixos configurations and the modules I use";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { nixpkgs } @ inputs: {
+    nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+      modules = [
+        ./configuration.nix
+      ];
+    };
   };
 }
