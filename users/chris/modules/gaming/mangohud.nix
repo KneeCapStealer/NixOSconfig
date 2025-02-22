2e4871f21a979c8855e30063ca1584700c6d21fc
Christoffer Hald Christensen <chrishald@proton.me>
2025-02-22 22:52:23 +0100

    updated mangohud settings

--- users/chris/modules/gaming/mangohud.nix
+++ users/chris/modules/gaming/mangohud.nix
@@ -1,3 +1,12 @@
 {
   programs.mangohud.enable = true;
+  programs.mangohud.settings = {
+    fps_metrics = "avg,0.01";
+    core_load = true;
+    cpu_stats = true;
+    gpu_stats = true;
+    frame_timing = true;
+    fps = true;
+  };
+
 }
