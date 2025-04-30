14f5040cd6b84a3111471b6240657b4c1f159666
Christoffer Hald Christensen <chrishald@proton.me>
2025-04-30 23:57:42 +0200

    using sudo-rs

--- nixos-configurations/desktop/users/default.nix
+++ nixos-configurations/desktop/users/default.nix
@@ -4,4 +4,11 @@
 
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
+
+  security.sudo.enable = false;
+  security.sudo-rs = {
+    enable = true;
+    wheelNeedsPassword = true;
+    execWheelOnly = true;
+  };
 }
