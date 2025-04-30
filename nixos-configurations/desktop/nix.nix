8ca255055fb5d82461ffedda259d843cf224efb0
Christoffer Hald Christensen <chrishald@proton.me>
2025-04-30 23:58:39 +0200

    Removed unused system-feature

--- nixos-configurations/desktop/nix.nix
+++ nixos-configurations/desktop/nix.nix
@@ -1,21 +1,24 @@
 { pkgs, ... }:
 {
   nix = {
-    settings.experimental-features = [
-      "flakes"
-      "nix-command"
-    ];
-    settings.system-features = [ "gccarch-x86-64-v3" ];
+    settings = {
+      experimental-features = [
+        "flakes"
+        "nix-command"
+      ];
 
-    settings.trusted-users = [
-      "root"
-      "@wheel"
-    ];
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+
+      cores = 6;
+      max-jobs = 4;
+    };
 
     package = pkgs.nixVersions.latest;
   };
 
   programs.nix-ld.enable = true;
-
   services.envfs.enable = true;
 }
