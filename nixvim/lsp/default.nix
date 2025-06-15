e08adbb93e89397a902aea4849f6539d98a21940
Christoffer Hald Christensen <chrishald@proton.me>
2025-06-16 01:41:55 +0200

    Updated keybindings

--- nixvim/lsp/default.nix
+++ nixvim/lsp/default.nix
@@ -10,23 +10,24 @@
       enable = true;
       inlayHints = true;
       capabilities = ''
-        local old = capabilities
-        capabilities = require("cmp_nvim_lsp").default_capabilities(old)
+        capabilities = require("cmp_nvim_lsp").default_capabilities()
       '';
 
       onAttach = ''
         local opts = { noremap = false, silent = true, buffer = bufnr }
+        local lsp = vim.lsp
 
-        vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set('n', '<leader>ws', vim.lsp.buf.workspace_symbol, opts)
+        vim.keymap.set('n', 'gd', lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', lsp.buf.hover, opts)
+        vim.keymap.set('n', '<leader>ws', lsp.buf.workspace_symbol, opts)
         vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
         vim.keymap.set('n', '<S-d>', vim.diagnostic.goto_next, opts)
         vim.keymap.set('n', '<A-d>', vim.diagnostic.goto_prev, opts)
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', '<leader>rf', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<leader>rr', vim.lsp.buf.rename, opts)
-        vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<leader>ca', lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>rf', lsp.buf.references, opts)
+        vim.keymap.set('n', '<leader>rr', lsp.buf.rename, opts)
+        vim.keymap.set('i', '<C-h>', lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<leader>ih', function () lsp.inlay_hint.enable(not lsp.inlay_hint.is_enabled()) end, opts)
       '';
 
       servers = {
