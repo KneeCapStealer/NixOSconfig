d7f687a2f731036d95328a0d9e69046633093bd5
Christoffer Hald Christensen <chrishald@proton.me>
2025-08-31 16:53:08 +0200

    Added trashy to avoid destroying my entire system again :)

--- nixos-configurations/desktop/users/default.nix
+++ nixos-configurations/desktop/users/default.nix
@@ -5,6 +5,16 @@
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
+  environment.systemPackages = with pkgs; [
+    trashy
+  ];
+
+  programs.zsh.shellAliases = {
+    rm = "trash put";
+    rmlist = "trash list";
+    unrm = "trash restore";
+  };
+
   security.sudo.enable = false;
   security.sudo-rs = {
     enable = true;
