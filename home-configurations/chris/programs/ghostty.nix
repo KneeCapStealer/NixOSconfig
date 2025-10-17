9e68685f4c426eee382b1b4d02a1239c7094950c
Christoffer Hald Christensen <chrishald@proton.me>
2025-10-17 12:06:24 +0200

    Ghostty transparent background

--- home-configurations/chris/programs/ghostty.nix
+++ home-configurations/chris/programs/ghostty.nix
@@ -2,6 +2,9 @@
   programs.ghostty.enable = true;
   programs.ghostty.settings = {
     font-size = 12;
+    background-opacity = 0.8;
+    background-opacity-cells = true;
+    background-blur = true;
   };
 
   home.sessionVariables = {
