b479389f76df4994a0d82e11f5519e9bc8ca5881
Christoffer Hald Christensen <chrishald@proton.me>
2025-11-26 13:15:08 +0100

    nixvim: Removed overwritten hotkeys

--- nixvim/lsp/default.nix
+++ nixvim/lsp/default.nix
@@ -11,6 +11,7 @@ in
 
   plugins = {
     cmp-nvim-lsp.enable = true;
+    none-ls.enable = true;
     lspconfig = {
       enable = true;
     };
@@ -22,15 +23,12 @@ in
       local opts = { noremap = false, silent = true, buffer = bufnr }
       local lsp = vim.lsp
 
-      vim.keymap.set('n', 'gd', lsp.buf.definition, opts)
       vim.keymap.set('n', 'K', lsp.buf.hover, opts)
       vim.keymap.set('n', '<leader>ws', lsp.buf.workspace_symbol, opts)
       vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
       vim.keymap.set('n', '<S-d>', vim.diagnostic.goto_next, opts)
       vim.keymap.set('n', '<A-d>', vim.diagnostic.goto_prev, opts)
       vim.keymap.set('n', '<leader>ca', lsp.buf.code_action, opts)
-      vim.keymap.set('n', '<leader>rf', lsp.buf.references, opts)
-      vim.keymap.set('n', '<leader>rr', lsp.buf.rename, opts)
       vim.keymap.set('i', '<C-h>', lsp.buf.signature_help, opts)
       vim.keymap.set('n', '<leader>ih', function () lsp.inlay_hint.enable(not lsp.inlay_hint.is_enabled()) end, opts)
       vim.keymap.set('n', '<leader>rn', lsp.buf.rename, opts)
