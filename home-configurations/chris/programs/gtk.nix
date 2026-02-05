b6d2b11b55342fd1b931a801712e776077f10b6f
Christoffer Hald Christensen <chrishald@proton.me>
2026-02-05 15:08:39 +0100

    fcitx5 for gtk input, because of ghostty bug

--- home-configurations/chris/programs/gtk.nix
+++ home-configurations/chris/programs/gtk.nix
@@ -1,6 +1,16 @@
+{ pkgs, ... }:
 {
   gtk = {
     enable = true;
     theme.name = "Adwaita:dark";
+  };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-gtk
+      catppuccin-fcitx5
+    ];
   };
 }
