d07b03875641b9b89dba4ba3bc7cb8436f0b5e20
Christoffer Hald Christensen <chrishald@proton.me>
2026-02-06 17:18:53 +0100

    laptop: disable nvidia offload

--- nixos-configurations/laptop/hardware/nvidia.nix
+++ nixos-configurations/laptop/hardware/nvidia.nix
@@ -26,9 +26,6 @@
     prime = {
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
-      # sync.enable = true;
-      offload.enable = true;
-      offload.enableOffloadCmd = true;
     };
   };
 
