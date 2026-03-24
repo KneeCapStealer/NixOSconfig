98713ad4b6e2478da31fb941726b22d87aa41276
Christoffer Hald Christensen <chrishald@proton.me>
2026-03-24 21:48:17 +0100

    all hosts: Fixed environment GTK misspelled as GDK

--- nixos-configurations/laptop/hyprland/environment.nix
+++ nixos-configurations/laptop/hyprland/environment.nix
@@ -11,7 +11,7 @@
       QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
 
-      GDK_BACKEND = "wayland,x11,*";
+      GTK_BACKEND = "wayland,x11,*";
       SDL_VIDEODRIVER = "wayland";
       CLUTTER_BACKEND = "wayland";
 
