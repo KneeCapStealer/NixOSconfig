745791a73552c62d9c1a2660757e1ea2d86d7da0
Christoffer Hald Christensen <chrishald@proton.me>
2026-03-24 21:47:29 +0100

    desktop: fix languages for desktop

--- nixos-configurations/desktop/danish.nix
+++ nixos-configurations/desktop/danish.nix
@@ -2,18 +2,12 @@
 {
   i18n = {
     defaultLocale = "en_DK.UTF-8";
-    extraLocales = [
-      "da_DK.UTF-8/UTF-8"
-      "en_DK.UTF-8/UTF-8"
-      "en_US.UTF-8/UTF-8"
-
-      "da_DK/ISO-8859-1"
-      "en_DK/ISO-8859-1"
-      "en_US/ISO-8859-1"
-    ];
     extraLocaleSettings = {
+      LANGUAGE = "da";
+      LC_MESSAGES = "en_DK.UTF-8";
+      # LC_ALL = "da_DK.UTF-8"; # This overrides all other LC_* settings.
+      LC_CTYPE = "da_DK.UTF-8";
       LC_ADDRESS = "da_DK.UTF-8";
-      LC_IDENTIFICATION = "da_DK.UTF-8";
       LC_MEASUREMENT = "da_DK.UTF-8";
       LC_MONETARY = "da_DK.UTF-8";
       LC_NAME = "da_DK.UTF-8";
@@ -21,25 +15,33 @@
       LC_PAPER = "da_DK.UTF-8";
       LC_TELEPHONE = "da_DK.UTF-8";
       LC_TIME = "da_DK.UTF-8";
+      LC_COLLATE = "da_DK.UTF-8";
     };
-
+    extraLocales = [ "all" ];
     inputMethod = {
       enable = true;
       type = "fcitx5";
-      fcitx5.addons = with pkgs; [
-        fcitx5-gtk
-        catppuccin-fcitx5
-      ];
-      fcitx5.settings.inputMethod = {
-        GroupOrder."0" = "Default";
-        "Groups/0" = {
-          Name = "Default";
-          "Default Layout" = "dk";
+      fcitx5 = {
+        waylandFrontend = true;
+        ignoreUserConfig = true;
+        addons = with pkgs; [
+          fcitx5-gtk
+          catppuccin-fcitx5
+        ];
+
+        settings.inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "dk";
+          };
+          "Groups/0/Items/0".Name = "keyboard-dk";
         };
-        "Groups/0/Items/0".Name = "keyboard-dk";
       };
     };
   };
+  environment.sessionVariables.QT_IM_MODULES = "wayland;fcitx;ibus";
+
   time.timeZone = "Europe/Copenhagen";
 
   services.xserver.xkb = {
