6b25c8ccbb6e0310057606ba122e2d0271bff033
Christoffer Hald Christensen <chrishald@proton.me>
2026-03-25 15:49:51 +0100

    laptop: Copied desktop danish config

--- nixos-configurations/laptop/danish.nix
+++ nixos-configurations/laptop/danish.nix
@@ -1,14 +1,13 @@
+{ pkgs, ... }:
 {
   i18n = {
     defaultLocale = "en_DK.UTF-8";
-    extraLocales = [
-      "da_DK/ISO-8859-1"
-      "en_DK/ISO-8859-1"
-      "en_US/ISO-8859-1"
-    ];
     extraLocaleSettings = {
+      LANGUAGE = "da";
+      LC_MESSAGES = "en_DK.UTF-8";
+      # LC_ALL = "da_DK.UTF-8"; # This overrides all other LC_* settings.
+      LC_CTYPE = "da_DK.UTF-8";
       LC_ADDRESS = "da_DK.UTF-8";
-      LC_IDENTIFICATION = "da_DK.UTF-8";
       LC_MEASUREMENT = "da_DK.UTF-8";
       LC_MONETARY = "da_DK.UTF-8";
       LC_NAME = "da_DK.UTF-8";
@@ -16,8 +15,32 @@
       LC_PAPER = "da_DK.UTF-8";
       LC_TELEPHONE = "da_DK.UTF-8";
       LC_TIME = "da_DK.UTF-8";
+      LC_COLLATE = "da_DK.UTF-8";
+    };
+    extraLocales = [ "all" ];
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5 = {
+        waylandFrontend = true;
+        ignoreUserConfig = true;
+        addons = with pkgs; [
+          fcitx5-gtk
+          catppuccin-fcitx5
+        ];
+
+        settings.inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "dk";
+          };
+          "Groups/0/Items/0".Name = "keyboard-dk";
+        };
+      };
     };
   };
+  environment.sessionVariables.QT_IM_MODULES = "wayland;fcitx;ibus";
 
   time.timeZone = "Europe/Copenhagen";
 
@@ -26,8 +49,15 @@
   };
 
   console = {
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+    keyMap = "us";
+    # prevents `systemd-vconsole-setup` failing during systemd initrd
+    earlySetup = true;
+  };
+  systemd.services.systemd-vconsole-setup.unitConfig.After = "local-fs.target";
+
+  boot.initrd = {
     enable = true;
-    keyMap = "dk-latin1";
-    # font = "FiraCode Nerd Font Mono";
+    systemd.enable = true;
   };
 }
