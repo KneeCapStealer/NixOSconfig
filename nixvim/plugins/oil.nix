352f6e128a0c824da5ec3e5746922b56adccb66d
Christoffer Hald Christensen <chrishald@proton.me>
2026-05-10 15:33:46 +0200

    feat(nixvim): oil hidden files keybind

--- nixvim/plugins/oil.nix
+++ nixvim/plugins/oil.nix
@@ -1,3 +1,7 @@
+{ lib, ... }:
+let
+  helpers = lib.nixvim;
+in
 {
   plugins.oil.enable = true;
   plugins.oil.settings = {
@@ -10,6 +14,12 @@
       action = "<cmd>Oil<CR>";
       key = "<leader>e";
       options.silent = true;
+      options.desc = "Open the Oil file explorer";
+    }
+    {
+      action = helpers.mkRaw "function() require('oil').toggle_hidden() end";
+      key = "H";
+      options.desc = "Toggle hidden files";
     }
   ];
 }
