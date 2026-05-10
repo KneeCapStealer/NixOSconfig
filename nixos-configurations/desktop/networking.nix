6058e7bcf1e321448b382bfd94859a340b2ceb6b
Christoffer Hald Christensen <chrishald@proton.me>
2026-05-10 15:39:57 +0200

    desktop(networking): cleanup networking.nix

--- nixos-configurations/desktop/networking.nix
+++ nixos-configurations/desktop/networking.nix
@@ -9,7 +9,7 @@
   ];
 
   networking.nftables.enable = true;
-
+  networking.wireless.iwd.enable = true;
   networking.networkmanager = {
     enable = true;
     wifi.backend = "iwd";
@@ -22,30 +22,5 @@
     };
   };
 
-  networking.wireless.iwd.enable = true;
 
-  # qbittorrent
-  networking.firewall.allowedTCPPorts = [ 7640 ];
-  networking.firewall.allowedUDPPorts = [ 7640 ];
-
-  services.nextDNS = {
-    enable = false;
-    endpointId = "f8ac75";
-    deviceName = "Chris--Desktop";
-  };
-
-  services.protonVPN = {
-    enable = false;
-
-    router.defaultRoute = "192.168.1.1 dev enp8s0";
-    interface = {
-      privateKeyFile = "/root/secrets/protonVPN.key";
-      address = "10.2.0.2/32";
-
-      peer = {
-        publicKey = "+6VseaiwdWLFSlaTgwafQM9D9DsT1aanqywtgf7XdC8=";
-        endpoint.address = "185.111.109.1";
-      };
-    };
-  };
 }
