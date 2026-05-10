0cab2e4aeedf770e899265778345f3ff80c84e4f
Christoffer Hald Christensen <chrishald@proton.me>
2026-05-10 15:41:52 +0200

    chris(dev): git amend alias

--- home-configurations/chris/dev/default.nix
+++ home-configurations/chris/dev/default.nix
@@ -22,14 +22,13 @@
       };
 
       alias = {
-        amend = "commit --amend -a";
-        b = "branch";
+        amend = "commit --amend --no-edit --date=now";
         yank = "pull";
+        yeet = "push";
         st = "status -sb";
         lg1 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
         lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
         lg = "lg1";
-        fix = "commit --ammend --no-edit --date=now";
         force = "push --force-with-lease --force-if-includes";
         patch = "add --patch";
         rank = "shortlog --summary --numbered --no-merges";
