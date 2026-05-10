478c373f8e34b392f31e604113b0605040101a34
Christoffer Hald Christensen <chrishald@proton.me>
2026-05-10 15:42:30 +0200

    chris(mpv): add desktop specific MPV config

--- home-configurations/chris/programs/mpv.nix
+++ home-configurations/chris/programs/mpv.nix
@@ -1,5 +1,32 @@
+{ host, lib, ... }:
 {
   programs.mpv.enable = true;
-  programs.mpv.config = {
-  };
+  programs.mpv.config = lib.mkMerge [
+    (lib.mkIf (host == "desktop") {
+      profile = "high-quality";
+      fullscreen = "yes";
+      save-position = "yes";
+      cursor-autohide = 200;
+
+      # Audio language prio
+      alang = "ja,jp,jpn,en,eng";
+
+      # Subtitle language prio
+      slang = "en,eng";
+
+      vo = "gpu-next";
+      gpu-api = "vulkan";
+      hwdec = "vulkan";
+
+      dither-depth = 10;
+      dither = "error-diffusion";
+      error-diffusion = "floyd-steinberg";
+
+      target-colorspace-hint = "yes";
+      target-contrast = "inf";
+      target-prim = "display-p3";
+      target-trc = "pq";
+      target-peak = 1000;
+    })
+  ];
 }
