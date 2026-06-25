72f7357e7d3015b9f170a5fd8c43dd9bce441d6f
Christoffer Hald Christensen <chrishald@proton.me>
2026-06-25 15:09:03 +0200

    chris: removed vesktop and vencord config

--- home-configurations/chris/programs/vesktop.nix
+++ home-configurations/chris/programs/vesktop.nix
@@ -1,18 +1,7 @@
 {
   programs.vesktop = {
     enable = true;
-    vencord.useSystem = true;
-    vencord.settings = {
-      autoUpdate = false;
-      autoUpdateNotification = false;
-      notifyAboutUpdates = false;
-      useQuickCss = true;
-      disableMinSize = true;
-      arRPC = true;
-      plugins = {
-        petpet.enabled = true;
-      };
-    };
   };
+  catppuccin.vesktop.enable = false;
   services.arrpc.enable = true;
 }
