ba1442f253f2a3f71f43477c3f96a97d35287939
Christoffer Hald Christensen <chrishald@proton.me>
2026-06-25 14:50:17 +0200

    feat(templates): add devshell template

---
+++ templates/devShell/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      lib = nixpkgs.lib;
+      forAllSystems = f: lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.nixfmt);
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            # Packages here :)
+          ];
+        };
+      });
+    };
+}
